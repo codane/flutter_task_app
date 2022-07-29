27cb66929fba3cc19e9a1c3233e1a27b8e98f07f
babic <babdan5@gmail.com>
2022-07-29 21:55:37 +0200

    added basic second page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_task_app/add_task_page.dart';
 import 'task_list_widget.dart';
+import 'top_custom_clipper.dart';
 
 void main() {
-  runApp(const TaskApp());
+  runApp(const MaterialApp(
+    home: TaskApp(),
+  ));
 }
 
 class TaskApp extends StatelessWidget {
@@ -11,10 +15,36 @@ class TaskApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: TaskListWidget(),
-        ),
+    return Scaffold(
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          ClipPath(
+            clipper: TopCustomClipper(),
+            child: Container(
+              height: 200,
+              color: Colors.amber,
+            ),
+          ),
+          const Text(
+            "List of tasks",
+            style: TextStyle(
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          const Expanded(
+            child: TaskListWidget(),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => const AddTaskPage()));
+        },
+      ),
     );
   }
 }
